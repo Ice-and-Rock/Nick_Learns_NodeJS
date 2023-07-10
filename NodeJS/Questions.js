efe4cda2e67ad3a8ec344bb440bcf6f47270b58f
nick v <nvalente.electrics@gmail.com>
2023-07-10 14:43:41 +0100

    created a timer the goes in 500ms intervals. stops at 3 secs

--- NodeJS/Questions.js
+++ NodeJS/Questions.js
@@ -38,44 +38,69 @@
 // __________________________________________________________________
 
 
-process.stdout.write("hello \n \n");
+// // BELOW ***
+// // create a small question list, take in answers then return the answers in a synamic string
 
-// Set the array of Q's
-const questions = [
-  "what is your name?",
-  "what would you rather being doing?",
-  "what is your rpefered programming language?",
-];
-// Set the empty A's array
-const answers = [];
+// process.stdout.write("hello \n \n");
 
-function ask(i = 0) {
-  process.stdout.write(`\n \n \n ${questions[i]}`);
-  process.stdout.write(` > `);
-}
+// // Set the array of Q's
+// const questions = [
+//   "what is your name?",
+//   "what would you rather being doing?",
+//   "what is your rpefered programming language?",
+// ];
+// // Set the empty A's array
+// const answers = [];
 
-// call the answer
-ask();
+// function ask(i = 0) {
+//   process.stdout.write(`\n \n \n ${questions[i]}`);
+//   process.stdout.write(` > `);
+// }
 
-// Wait for an answer
-process.stdin.on("data", function (data) {
-  answers.push(data.toString().trim());
-  if (answers.length < questions.length) {
-    ask(answers.length);
-  } else {
-    process.exit();
-  }
-});
+// // call the answer
+// ask();
 
-// input the answers from the array in a dynamic string
-process.on("exit", function () {
-  process.stdout.write("\n\n\n\n");
-  process.stdout.write(
-    `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`
-  );
-});
+// // call this function on a 'data' event
+// // Wait for an answer
+// process.stdin.on("data", function (data) {
+//   answers.push(data.toString().trim());
+//   if (answers.length < questions.length) {
+//     ask(answers.length);
+//   } else {
+//     process.exit();
+//   }
+// });
+
+// // call this function on the 'exit' event
+// // input the answers from the array in a dynamic string
+// process.on("exit", function () {
+//   process.stdout.write("\n\n\n\n");
+//   process.stdout.write(
+//     `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`
+//   );
+// });
 
 
 // __________________________________________________________________
 
+// BELOW ***
+// Create a timer counting in 500ms delays logged to the console
 
+const waitTime = 3000;
+console.log(`setting a ${waitTime/1000} second delay`);
+const timerFinished = () => {
+    clearInterval(interval)
+    console.log("done");
+};
+
+setTimeout(timerFinished, waitTime);
+
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+};
+
+const interval = setInterval(incTime, waitInterval);
